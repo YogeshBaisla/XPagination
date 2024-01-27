8cf6823394516da5ad63c5ac97da1aa8c6ffd107
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-01-27 13:21:15 +0530

    Pushing x[agination code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import {useEffect,useState} from "react"
+
 
 function App() {
+  const [employees,setEmployee] = useState([])
+  const [pageNumber,setPageNumber] = useState(1)
+  const [rowEmployee,setrowEmployee] = useState([])
+  useEffect(()=>{
+    const fetchEmployee = async()=>{
+      try{
+      let data = await axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      console.log(data.data)
+      setEmployee(data.data)
+    }
+      catch(err){
+        console.log("Error in fetching data",err)
+        alert("failed to fetch data")
+      }
+    }
+    fetchEmployee()
+  },[])
+  useEffect(()=>{
+    // b = a.map((e,index)=>({[index+1]:e}))
+    let rowEmp = [];
+for (let i = 0; i < employees.length; i += 10) {
+  rowEmp.push(employees.slice(i, i + 10));
+}
+    setrowEmployee(rowEmp)
+  },[employees])
+  const tableRow ={
+    background:"#03fc7b",
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div><h1>Employee Data Table</h1></div>
+      <div>
+        <table>
+          <thead>
+          <tr style={tableRow}>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+          </thead>
+          <tbody>
+          {rowEmployee.length !== 0 && rowEmployee[pageNumber-1].map((employee)=>(
+            <tr key={employee.id}>
+            <td>{employee.id}</td>
+            <td>{employee.name}</td>
+            <td>{employee.email}</td>
+            <td>{employee.role}</td>
+          </tr>
+          ))}
+          </tbody>
+        </table>
+        <div>
+          <button onClick={() => setPageNumber(prevValue => prevValue - 1)} disabled={pageNumber === 1}>Previous</button>
+          <span>{pageNumber}</span>
+          <button onClick={() => setPageNumber(prevValue => prevValue + 1)} disabled={pageNumber === rowEmployee.length}>Next</button>
+        </div>
+      </div>
+      
     </div>
   );
 }
